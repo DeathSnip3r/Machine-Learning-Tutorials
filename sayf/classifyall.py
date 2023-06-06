bcc350ca940525023b91e52f4ec675679430f11a
Super Sayf <sayfullah.jumoorty@live.com>
2023-06-06 14:20:34 +0200

    classifyall

--- sayf/classifyall.py
+++ sayf/classifyall.py
@@ -1,33 +1,26 @@
-# import docker
-# import random
-import sys
+import numpy as np
+from sklearn.metrics import accuracy_score
+from tensorflow import keras
 
-# def build_image(docker_client, tag):
-#     image_tag = "rl_assignment_" + str(tag)
-#     path = "./"
-#     docker_client.images.build(path=path, tag=image_tag)
-#     return image_tag
+# Load the h5 model and test data
+model = keras.models.load_model("58-1.h5")
+test_data = np.genfromtxt("traindata.txt", delimiter=",")
 
- 
-if __name__ == '__main__':
-    with open('readme.txt', 'w') as f:
-        f.write('red\n')
-        f.write('red\n')
-        f.write('red\n')
-        f.write('red\n')
-        f.write('red\n')
-        f.write('red\n')
-        f.write('yellow\n')
-        f.write('red\n')
-    f.close()
-    # docker_client = docker.from_env()
-    # tag_number = 7307  # random.randint(0, 9999)
-    # image_tag = build_image(docker_client, tag_number)
+# Run the model on the test data
+predictions = model.predict(test_data)
 
-    # result_lines = docker_client.containers.run(image_tag, name=image_tag,
-    #                                             stderr=True,
-    #                                             remove=True,
-    #                                             mem_limit="2GB")
-    # result_lines = result_lines.splitlines()
-    # if len(result_lines) > 0:
-    #     print(float(result_lines[-1]))
+# Save the predicted labels to a file as integers
+predicted_labels = np.argmax(predictions, axis=1)
+np.savetxt("predicted_labels.txt", predicted_labels, fmt="%d")
+
+############################################################################
+
+# Take in the trainlabels.txt file and the predicted_labels.txt file
+# and print the accuracy score as a percentage
+true_labels = np.genfromtxt("trainlabels.txt", delimiter=",")
+predicted_labels = np.genfromtxt("predicted_labels.txt", delimiter=",")
+print(
+    "Model accuracy: {:.2f}%".format(
+        accuracy_score(true_labels, predicted_labels) * 100
+    )
+)
